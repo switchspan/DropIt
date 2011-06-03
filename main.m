9fbde3efc8f9dcf2a7f3cda4a08c540b0c62af16
Ken Taylor <taylor.kenneth@gmail.com>
2011-06-03 03:23:48 -0400

    first commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DropIt
+//
+//  Created by Ken Taylor on 6/3/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
